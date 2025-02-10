606e0d1da99f9e21a31887807e14e2f78dab2f44
mohamady-div <major_2047@hotmail.com>
2025-02-10 20:02:53 +0200

    Lesson 11

---
+++ AspMVCProject/Controllers/EmailController.cs
@@ -0,0 +1,37 @@
+﻿using AspMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace AspMVCProject.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Index()
+        {
+            mdlEmail mdl = new mdlEmail();
+            return View(mdl);
+        }
+
+        [HttpPost]
+        public IActionResult Index(mdlEmail mdl)
+        {
+            MailMessage mailMessage = new MailMessage
+            {
+                From = new MailAddress(mdl.senderEmail!),
+                Subject = mdl.subject,
+                Body = mdl.body,
+                IsBodyHtml = false
+            };
+
+            SmtpClient smtpClient = new SmtpClient(mdl.smtpServer, mdl.smtpPort)
+            {
+                Credentials = new NetworkCredential(mdl.senderEmail, mdl.senderPassword),
+                EnableSsl = true
+            };
+
+            smtpClient.Send(mailMessage);
+            return View("index");
+        }
+    }
+}
---
+++ AspMVCProject/Models/mdlEmail.cs
@@ -0,0 +1,20 @@
+﻿namespace AspMVCProject.Models
+{
+    public class mdlEmail
+    {
+        public string? smtpServer { get; set; } = "smtp.gmail.com";
+
+        public int smtpPort { get; set; } = 587;
+
+        public string? senderEmail { get; set; }
+
+        public string? senderPassword { get; set; }
+
+        public string? recipientEmail { get; set; }
+
+        public string? subject { get; set; }
+
+        public string? body { get; set; }
+
+    }
+}
