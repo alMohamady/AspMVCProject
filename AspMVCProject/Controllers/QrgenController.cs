397b7536813f4008770e78cbe20966160416ecdc
mohamady-div <major_2047@hotmail.com>
2025-02-10 19:37:54 +0200

    Lesson 10

--- AspMVCProject/Controllers/QrgenController.cs
+++ AspMVCProject/Controllers/QrgenController.cs
@@ -16,7 +16,26 @@ namespace AspMVCProject.Controllers
         [HttpPost]
         public IActionResult Index(mdlQrCoder mdl)
         {
-            Payload payload = new Url(mdl.ImageUrl ?? "");
+            Payload? payload = null;
+
+            switch (mdl.QrCodeType)
+            {
+                case 1: //url
+                    payload = new Url(mdl.ImageUrl ?? "");
+                    break;
+                case 2: //sms
+                    payload = new SMS(mdl.smsPhoneNumber!, mdl.smsBody!);
+                    break;
+                case 3: //waap
+                    payload = new WhatsAppMessage(mdl.wapNumber!, mdl.wapMessage!);
+                    break;
+                case 4: //mail
+                    payload = new Mail(mdl.Email, mdl.EmailSubject!, mdl.EmailBody);
+                    break;
+                case 5: //wifi
+                    payload = new WiFi(mdl.wifiName!, mdl.wifiPassword!, WiFi.Authentication.WPA);
+                    break;
+            }
 
             QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
             QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(payload);
--- AspMVCProject/Models/mdlQrCoder.cs
+++ AspMVCProject/Models/mdlQrCoder.cs
@@ -6,11 +6,24 @@
 
         public string? ImageUrl { get; set; }
 
-        public string? QrImageUrl { get; set; }
+        public string? smsPhoneNumber { get; set; }
+
+        public string? smsBody { get; set; }
+
+        public string? wapNumber { get; set; }
+
+        public string? wapMessage{ get; set; }
 
         public string? Email { get; set; }
 
-        public string? QrEmail { get; set; }
+        public string? EmailSubject { get; set; }
+
+        public string? EmailBody{ get; set; }
+
+        public string? wifiName { get; set; }
+        public string? wifiPassword { get; set; }
+
+        public string? QrImageUrl { get; set; }
 
     }
 }
