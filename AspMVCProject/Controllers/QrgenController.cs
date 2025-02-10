6cc56ddde3aa8d14bde8c35f44204f927df4eba5
mohamady-div <major_2047@hotmail.com>
2025-02-10 19:12:45 +0200

    Lesson 9

---
+++ AspMVCProject/Controllers/QrgenController.cs
@@ -0,0 +1,31 @@
+﻿using AspMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using static QRCoder.PayloadGenerator;
+
+namespace AspMVCProject.Controllers
+{
+    public class QrgenController : Controller
+    {
+        public IActionResult Index()
+        {
+            mdlQrCoder mdl = new mdlQrCoder();
+            return View(mdl);
+        }
+
+        [HttpPost]
+        public IActionResult Index(mdlQrCoder mdl)
+        {
+            Payload payload = new Url(mdl.ImageUrl ?? "");
+
+            QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(payload);
+
+            BitmapByteQRCode qRCode = new BitmapByteQRCode(qRCodeData);
+            string base64String = Convert.ToBase64String(qRCode.GetGraphic(20));
+            mdl.QrImageUrl = "data:image/png;base64," + base64String;
+
+            return View("Index", mdl);
+        }
+    }
+}
---
+++ AspMVCProject/Models/mdlQrCoder.cs
@@ -0,0 +1,16 @@
+﻿namespace AspMVCProject.Models
+{
+    public class mdlQrCoder
+    {
+        public int QrCodeType { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? QrImageUrl { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? QrEmail { get; set; }
+
+    }
+}
